d9e28c22b2c46d74d76b41369aebd093aa6bef14
timurziev <storona77@gmail.com>
2018-09-05 00:15:02 +0300

    create database connection

---
+++ App/Models/Model.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use PDO;
+use Config\Database;
+
+abstract class Model
+{
+
+    /**
+     * Connect to database
+     *
+     * @return mixed
+     */
+    protected static function DB()
+    {
+        $database = 'mysql:host=' . Database::DB_HOST . ';dbname=' . Database::DB_NAME . ';charset=utf8';
+        $db = new PDO($database, Database::DB_USER, Database::DB_PASSWORD);
+
+        // In case of error return an exception
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        return $db;
+    }
+}
---
+++ Config/Database.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Config;
+
+class Database {
+
+    /**
+     * Database host
+     * @var string
+     */
+    const DB_HOST = 'localhost';
+
+    /**
+     * Database name
+     * @var string
+     */
+    const DB_NAME = 'test';
+
+    /**
+     * Database user
+     * @var string
+     */
+    const DB_USER = 'root';
+
+    /**
+     * Database password
+     * @var string
+     */
+    const DB_PASSWORD = '';
+}
